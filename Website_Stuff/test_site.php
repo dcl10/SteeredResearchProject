8f4a1f8f00e1edc6f5fb6f66f76bf7d5eb39e338
dcl10 <lea.daniel@hotmail.co.uk>
2017-06-26 18:19:29 +0100

    playing around

--- Website_Stuff/test_site.php
+++ Website_Stuff/test_site.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<form action="get_result.php" method="post">
+<form method="post">
 Gene: <input type="text" name="name"><br>
 <input type="submit">
 </form>
-
+    
 </body>
 </html>
