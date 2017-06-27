dffd112e0c29766a378b858026b8d2dcd7b9c18d
dcl10 <lea.daniel@hotmail.co.uk>
2017-06-27 10:54:43 +0100

    Can now query a database

--- Website_Stuff/test_site.php
+++ Website_Stuff/test_site.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
+    
+    <?php
+    $servername = "localhost";
+    $username = "dcl10";
+    $password = "1-Puddlejumper";
+    $db_name = "TestSeqDB";
+    
+    $conn = new mysqli($servername, $username, $password, $db_name);
+    
+    if ($conn -> connect_error) {
+        die ("Connection failed: " . $conn -> connect_error);
+    } else {
+        echo "Connection successful :)";
+    }
+    ?>
 
     <?php
     $output = "";
@@ -13,7 +28,20 @@
 Gene: <input type="text" name="name" value="<?php echo $output; ?>"><br>
 <input type="submit" name="submit">
 </form>
-    <?php echo $output;?>
+    <?php
+    $sql = "SELECT * FROM Experimental WHERE geneID = \"$output\"";
+    $result = $conn -> query($sql);
+    echo $sql . "<br>";
+    $table = "<table> <tr> <th>Gene ID</th><th>Fold Change</th> <tr>";
+    
+    if ($result -> num_rows > 0) {
+        while ($row = $result -> fetch_assoc()) {
+            $table .= "<tr><td>".$row["geneID"]."</td><td>".$row["foldChange"]."</td></tr>";  
+            //echo "gene: " .  $row["geneID"] . " " . "fold change: " . $row["foldChange"]."<br>";
+        }
+        echo $table;
+    } else echo "<strong>No results today</strong>";
+    ?>
     
 </body>
 </html>
