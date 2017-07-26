943d3c15770eef139cca2d1f8b80fe0fa08f2e62
CareyLM <clm79@le.ac.uk>
2017-07-26 11:51:42 +0100

    Remove error message
    Will no longer display long error message when no gene name is entered

--- shiny_web_app.R
+++ shiny_web_app.R
@@ -28,69 +28,67 @@ dbDisconnect(con)
 # UI (User Interface) part of Shiny App
 ui <- fluidPage(
   
-  # Title 
-  headerPanel("Gene Expression Between Samples"),
-  
   #Get gene -id - Currently Test box input - need to link to webpage
   textInput(inputId = "id", label = "Gene_Id"),
   
   # Show a plot 
   mainPanel(
-    plotOutput("plot"),
-    textOutput("urlText")
+    plotOutput("plot")
   )
 )
 
 #Server part of Shiny App
 #Define server function 
-server <- function(input, output, session) {
-  
-  output$urlText <- renderText(session$gene_id)
+server <- function(input, output) {
   
   output$plot <- renderPlot({
-  
-  Genome <- c(rep("rn4",5),rep("rn6",5))
-  Sample <- rep(c("Aflatoxin 1", "Aflatoxin 2", "Aflatoxin 3","Control 1", "Control 2"),2)
-  
-  #Load data for specific gene from tables
-  FPKM <- c(all_FPKM_rn4$AFL4_FPKM[all_FPKM_rn4$gene_name==input$id],+
-              all_FPKM_rn4$AFL6_FPKM[all_FPKM_rn4$gene_name==input$id],+
-              all_FPKM_rn4$AFL8_FPKM[all_FPKM_rn4$gene_name==input$id],+
-              all_FPKM_rn4$CTRL1_FPKM[all_FPKM_rn4$gene_name==input$id],+
-              all_FPKM_rn4$CTRL2_FPKM[all_FPKM_rn4$gene_name==input$id],+
-              all_FPKM_rn6$AFL4_FPKM[all_FPKM_rn6$gene_name==input$id],+
-              all_FPKM_rn6$AFL6_FPKM[all_FPKM_rn6$gene_name==input$id],+
-              all_FPKM_rn6$AFL8_FPKM[all_FPKM_rn6$gene_name==input$id],+
-              all_FPKM_rn6$CTRL1_FPKM[all_FPKM_rn6$gene_name==input$id],+
-              all_FPKM_rn6$CTRL2_FPKM[all_FPKM_rn6$gene_name==input$id])
-  
-  #Get lo and hi conf FPKM value from TopHat output 
-  FPKM_lo <- c(all_FPKM_rn4$AFL4_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$AFL6_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$AFL8_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$CTRL1_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$CTRL2_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn6$AFL4_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$AFL6_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$AFL8_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$CTRL1_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$CTRL2_FPKM_lo[all_FPKM_rn6$gene_name==input$id])
-  
-  FPKM_hi <- c(all_FPKM_rn4$AFL4_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$AFL6_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$AFL8_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$CTRL1_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn4$CTRL2_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
-                 all_FPKM_rn6$AFL4_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$AFL6_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$AFL8_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$CTRL1_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
-                 all_FPKM_rn6$CTRL2_FPKM_hi[all_FPKM_rn6$gene_name==input$id])
-  
-  #Create summary table of gene info
-  Summary <- data.table(Genome, Sample, FPKM, FPKM_lo, FPKM_hi)
-  
-  #Plot summary info as bar graph - use lo and hi to plot error bars 
+    
+    validate(
+      need(input$id != "", "Please enter the gene id (eg NM_001000000)")
+    )
+    
+    Genome <- c(rep("rn4",5),rep("rn6",5))
+    Sample <- rep(c("Aflatoxin 1", "Aflatoxin 2", "Aflatoxin 3","Control 1", "Control 2"),2)
+    
+    #Load data for specific gene from tables
+    FPKM <- c(all_FPKM_rn4$AFL4_FPKM[all_FPKM_rn4$gene_name==input$id],+
+                all_FPKM_rn4$AFL6_FPKM[all_FPKM_rn4$gene_name==input$id],+
+                all_FPKM_rn4$AFL8_FPKM[all_FPKM_rn4$gene_name==input$id],+
+                all_FPKM_rn4$CTRL1_FPKM[all_FPKM_rn4$gene_name==input$id],+
+                all_FPKM_rn4$CTRL2_FPKM[all_FPKM_rn4$gene_name==input$id],+
+                all_FPKM_rn6$AFL4_FPKM[all_FPKM_rn6$gene_name==input$id],+
+                all_FPKM_rn6$AFL6_FPKM[all_FPKM_rn6$gene_name==input$id],+
+                all_FPKM_rn6$AFL8_FPKM[all_FPKM_rn6$gene_name==input$id],+
+                all_FPKM_rn6$CTRL1_FPKM[all_FPKM_rn6$gene_name==input$id],+
+                all_FPKM_rn6$CTRL2_FPKM[all_FPKM_rn6$gene_name==input$id])
+    
+    #Get lo and hi conf FPKM value from TopHat output 
+    FPKM_lo <- c(all_FPKM_rn4$AFL4_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$AFL6_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$AFL8_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$CTRL1_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$CTRL2_FPKM_lo[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn6$AFL4_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$AFL6_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$AFL8_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$CTRL1_FPKM_lo[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$CTRL2_FPKM_lo[all_FPKM_rn6$gene_name==input$id])
+    
+    FPKM_hi <- c(all_FPKM_rn4$AFL4_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$AFL6_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$AFL8_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$CTRL1_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn4$CTRL2_FPKM_hi[all_FPKM_rn4$gene_name==input$id],+
+                   all_FPKM_rn6$AFL4_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$AFL6_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$AFL8_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$CTRL1_FPKM_hi[all_FPKM_rn6$gene_name==input$id],+
+                   all_FPKM_rn6$CTRL2_FPKM_hi[all_FPKM_rn6$gene_name==input$id])
+    
+    #Create summary table of gene info
+    Summary <- data.table(Genome, Sample, FPKM, FPKM_lo, FPKM_hi)
+    
+    #Plot summary info as bar graph - use lo and hi to plot error bars 
     ggplot(Summary, aes(x=Sample, y=FPKM, fill=Genome)) + 
       geom_bar(stat="identity", position=position_dodge()) +
       geom_errorbar(aes(ymin=FPKM_lo, ymax=FPKM_hi), width=.2,
